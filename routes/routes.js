16bd25cd852bb774a5e53d97a90917f68dbc7911
Thoross <brendan.betts@live.com>
2013-11-20 01:59:56 -0400

    Added infinite scrolling to the search page. Removed machine specific config options. Added a few templated jade files and the new screen template.

--- routes/routes.js
+++ routes/routes.js
@@ -8,3 +8,15 @@ app = module.parent.exports.app;
 app.get('/', function(req, res){
     res.render('../views/index');
 });
+
+app.get('/search', function(req, res){
+    res.render('../views/search');
+});
+
+app.get('/connect', function(req, res){
+    res.render('../views/connect');
+});
+
+app.get('/screen', function(req, res){
+    res.render('../views/screen');
+});
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var io = require('socket.io').listen(3001);
+
+var config = require('./config/config');
+var io = require('socket.io').listen(config.ioPort);
 var http = require('https');
 var express = require('express');
 var app = express();
@@ -8,18 +10,20 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname));
 
 module.exports.app = app;
 routes = require('./routes/routes');
 
-app.listen('3000');
+app.listen(config.serverPort);
 console.log('Listening on Port 3000');
 
 var twitchAPI = 'https://api.twitch.tv/kraken/search/streams?limit=25&offset=0&q=';
 
 io.sockets.on('connection', function(socket) {
     socket.on('twitch:search', function(options) {
-        var request = http.get(twitchAPI + options.query, function(res) {
+        var query = options.next != null ? options.next : twitchAPI + options.query;
+        var request = http.get(query, function(res) {
             var responseBody = '';
 
             res.on("data", function(chunk){
@@ -34,5 +38,9 @@ io.sockets.on('connection', function(socket) {
         request.on('error', function(e) {
             socket.emit('twitch:search:error', e);
         });
+    });
+
+    socket.on('twitch:play', function(stream){
+        socket.emit('play-stream', {channel_name: stream})
     });
 });
