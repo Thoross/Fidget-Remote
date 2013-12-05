c72aa9abe18fc11139243b5cdd11c9e6e21b89bd
Thoross <brendan.betts@live.com>
2013-12-05 17:08:06 -0400

    Partial screen functionality has been added.

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 'use strict';
 var fidgetStream = angular.module('fidgetStream', ['btford.socket-io','infinite-scroll']).
 config(['socketProvider', function(socketProvider) {
-    var socket = io.connect('http://localhost:3001');
+    var socket = io.connect('http://bbetts-wsl2:3001');
         socketProvider.ioSocket(socket);
 }]);
+
+fidgetStream.factory('embeddingService', function(){
+    return {
+        name: '',
+        status: '',
+        loaded:false,
+        objectData: '',
+        flashVars: '',
+        viewers: 0,
+        set: function(stream){
+            this.name =  stream.stream.display_name;
+            this.status = stream.stream.status;
+            this.loaded = true;
+            this.objectData =  'http://www.twitch.tv/widgets/live_embed_player.swf?channel='+ this.name;
+            this.flashVars = 'hostname=www.twitch.tv&channel=' + this.name + '&auto_play=true&start_volume=25';
+            this.viewers =  stream.viewers;
+        },
+        getChannel: function(){
+            return {
+                name: this.name,
+                status: this.status,
+                objectData: this.objectData,
+                viewers: this.viewers,
+                flashVars: this.flashVars,
+            };
+        },
+        getLoaded: function(){
+            return this.loaded;
+        }
+
+    }
+});
+
 
 fidgetStream.controller('searchController', ['$scope', 'socket', function($scope, socket) {
     $scope.streams = [];
@@ -36,11 +69,6 @@ fidgetStream.controller('searchController', ['$scope', 'socket', function($scope
             socket.emit('twitch:search', { next: $scope.nextQuery });
         }
     }
-
-    $scope.$on('play', function(event, name){
-        $scope.name = name;
-        $scope.play();
-    });
 }]);
 
 fidgetStream.directive('twitchStream', ['socket',function(socket) {
@@ -52,38 +80,58 @@ fidgetStream.directive('twitchStream', ['socket',function(socket) {
         templateUrl: '/js/templates/twitchStream.html',
         link: function(scope, element, attrs){
             element.bind('click', function(){
-                socket.emit('twitch:play', {stream: scope.stream.channel.display_name});
+                socket.emit('twitch:play', {stream: scope.stream.channel, viewers: scope.stream.viewers});
             });
         }
     }
 }]);
 
-fidgetStream.controller('screenController', ['$scope','socket',function($scope, socket){
+fidgetStream.controller('screenController', ['$scope','socket','embeddingService' , function($scope, socket, embeddingService){
+    $scope.service = embeddingService;
     $scope.channel = {};
-    $scope.channel.name = '';
     $scope.loaded = false;
-    $scope.channel.objectData = '';
-    $scope.channel.flashVars = '';
 
     socket.on('play-stream',function(stream){
         $scope.$apply(function(){
-            $scope.channel.name = stream.stream;
-            $scope.channel.objectData = 'http://www.twitch.tv/widgets/live_embed_player.swf?channel='+ $scope.channel.name;
-            $scope.channel.flashVars = 'hostname=www.twitch.tv&channel=' + $scope.channel.name + '&auto_play=true&start_volume=25';
-            $scope.loaded = true;
-            console.log('$scope.channel: '+$scope.channel);
-            console.log('$scope.loaded: '+ $scope.loaded);
+            $scope.service.set(stream);
+            $scope.loaded = $scope.service.getLoaded();
+            $scope.channel = $scope.service.getChannel();
         });
+        $scope.$broadcast('newStream', $scope.channel);
     });
 
 }]);
 
-fidgetStream.directive('screen', [function(){
+fidgetStream.directive('screen', ['embeddingService', '$route', function(embeddingService, $route){
     return {
         restrict:'E',
         scope:{
-          channel: '=channel'
+          channel: '=',
+          loaded: '&'
         },
-        templateUrl: '/js/templates/screen.html'
+        replace: true,
+        transclude: false,
+        templateUrl: '/js/templates/screen.html',
+        link: function(scope, element, attrs){
+            scope.$on('newStream', function(event, stream){
+                scope.$watch(function(channelChanged){
+                    var streamPlayer = element.find('object');
+                    console.log('Loaded: '+scope.loaded);
+                    /*element.remove(streamPlayer);
+                    element.append(function(){
+                    });*/
+                });
+
+
+            });
+        }
     };
+}]);
+
+fidgetStream.directive('player', [function(){
+    return{
+        restrict:'E',
+        scope: true,
+        templateUrl:'/js/templates/player.html'
+    }
 }]);
